3fa7056cecd6aeafc9157e27f4b454cae91dcbf4
soichichi <souichi.t425@gmail.com>
2021-09-02 21:43:40 +0900

    change settings.py

--- testsite/settings.py
+++ testsite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-    'blog.apps.AccountsConfig',
+    'blog.apps.AccountsConfig', #追加
 ]
 
 MIDDLEWARE = [
