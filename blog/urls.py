eb59f38dbc8feb76886a997cc6f062a86085a3eb
soichichi <souichi.t425@gmail.com>
2021-10-23 16:08:39 +0900

    change html and urls and css for blog app

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.conf.urls import url
+from django.conf.urls import url , include
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -11,4 +11,6 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('home/',views.home, name ='home'),
 ]
